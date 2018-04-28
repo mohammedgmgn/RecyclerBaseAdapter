3be595196617cb0f5417f0405c4eb88b62739eb8
mohammed mahmoud <mohammed.95mahmoud@gmail.com>
2018-04-28 19:53:24 +0200

    add recycelrView Dependency

---
+++ recyclerbaseadapter/src/main/java/com/mohammed/mahmoud/recyclerbaseadapter/base/MyCustomViewHolder.java
@@ -0,0 +1,10 @@
+package com.mohammed.mahmoud.recyclerbaseadapter.base;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class MyCustomViewHolder extends RecyclerView.ViewHolder {
+    public MyCustomViewHolder(View itemView) {
+        super(itemView);
+    }
+}
